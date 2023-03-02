512de7ad648ad869b55ba7c315725e249d445a6e
Pauthianlal Simte <lalsimte@gmail.com>
2023-03-03 01:17:44 +0530

    Add files via upload

---
+++ models/User.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+const userSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: false,
+  },
+  mobile: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  role: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  location: {
+    type: String,
+    required: true,
+  },
+});
+
+module.exports = mongoose.model("users", userSchema);
